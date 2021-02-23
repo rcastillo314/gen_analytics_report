fd8dd0177b25fcd8ddcc30ad5961056b20443ee1
rcastillo619 <leon_619@outlook.com>
2021-02-22 20:10:39 -0800

    add constants and local imports

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+# Python libraries
 from fpdf import FPDF
 
-var = 'report using typed literals'
+# Local imports
+from daily_counts import plot_daily_count_states, plot_daily_count_countries
+
+WIDTH = 210
+HEIGHT = 297
+
 pdf = FPDF()
 pdf.add_page()
 pdf.set_font('Arial', 'B', 16)
