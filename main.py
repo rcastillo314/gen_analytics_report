a1a2da077ab21cbed28f55d1b5a22e6ce5308861
rcastillo619 <leon_619@outlook.com>
2021-02-23 20:05:22 -0800

    add helper function for passing deaths params

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from fpdf import FPDF
 
 # Local imports
 from daily_counts import plot_daily_count_states, plot_daily_count_countries
+from helper import Mode
 
 WIDTH = 210
 HEIGHT = 297
@@ -12,12 +13,15 @@ pdf.add_page()
 pdf.set_font('Arial', 'B', 16)
 pdf.cell(40, 10, 'Hello world')
 
+states = ['New Hampshire', 'Massachusetts']
+
 plot_daily_count_states(
-    ['New Hampshire', 'Massachusetts'], filename='test.png')
+    states, filename='test.png')
 pdf.image('test.png', 5, 30, WIDTH/2-5)
 
-plot_daily_count_countries(
-    ['US', 'India'], filename='test2.png')
+
+plot_daily_count_states(
+    states, mode=Mode.DEATHS, filename='test2.png')
 pdf.image('test2.png', WIDTH/2+5, 30, WIDTH/2-5)
 
 pdf.output('test.pdf', 'F')
