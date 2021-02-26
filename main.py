a9939c5b15bf3536e9d94d77589497dcd84c8065
rcastillo619 <leon_619@outlook.com>
2021-02-25 18:07:02 -0800

    refactor script to function

--- main.py
+++ main.py
@@ -9,20 +9,30 @@ from helper import Mode
 WIDTH = 210
 HEIGHT = 297
 
-pdf = FPDF()
-pdf.add_page()
-pdf.set_font('Arial', 'B', 16)
-pdf.cell(40, 10, 'Hello world')
 
-states = ['New Hampshire', 'Massachusetts']
+def create_report(filename='tutorial.pdf'):
+    pdf = FPDF()
+    ''''First page'''
+    pdf.add_page()
+    pdf.set_font('Arial', 'B', 16)
+    pdf.cell(40, 10, 'Hello world')
 
-plot_daily_count_states(
-    states, filename='test.png')
-pdf.image('test.png', 5, 30, WIDTH/2-5)
+    '''Second Page'''
+    states = ['New Hampshire', 'Massachusetts']
+    plot_daily_count_states(states, filename='test.png')
+    pdf.image('test.png', 5, 30, WIDTH/2-5)
+
+    plot_daily_count_states(states, mode=Mode.DEATHS, filename='test2.png')
+    pdf.image('test2.png', WIDTH/2+5, 30, WIDTH/2-5)
+
+    plot_states(states, filename='test3.png')
+    pdf.image('test3.png', 5, 110, WIDTH/2-5)
+
+    plot_states(states, mode=Mode.DEATHS, filename='test4.png')
+    pdf.image('test4.png', WIDTH/2+5, 110, WIDTH/2-5)
+
+    pdf.output(filename, 'F')
 
 
-plot_daily_count_states(
-    states, mode=Mode.DEATHS, filename='test2.png')
-pdf.image('test2.png', WIDTH/2+5, 30, WIDTH/2-5)
-
-pdf.output('test.pdf', 'F')
+if __name__ == '__main__':
+    create_report('test.pdf')
